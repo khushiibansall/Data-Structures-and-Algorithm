eab85c4d96fb560d8db7c22716651f5ba38a1bac
khushi bansal <khushibansal1048@gmail.com>
2025-06-24 14:56:29 +0530

    Create Find K Indices.cpp

---
+++ Array/Find K Indices.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> findKDistantIndices(vector<int>& nums, int key, int k) {
+        int n = nums.size();
+        vector<int> result;
+        for (int j = 0; j < n; j++) {
+            if (nums[j] == key) {
+                int start = max(0, j - k);
+                int end = min(n - 1, j + k);
+                if (!result.empty() && result.back() >= start) {
+                    start = result.back() + 1;
+                }
+                for (int a = start; a <= end; a++) {
+                    result.push_back(a);
+                }
+            }
+        }
+        return result;
+    }
+};
