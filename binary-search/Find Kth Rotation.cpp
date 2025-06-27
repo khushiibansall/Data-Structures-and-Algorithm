b19c85644ce20f56deffe4de86cd6bf6b2044eef
khushi bansal <khushibansal1048@gmail.com>
2025-06-27 12:34:03 +0530

    Create Find Kth Rotation.cpp

---
+++ binary-search/Find Kth Rotation.cpp
@@ -0,0 +1,14 @@
+// User function template for C++
+class Solution {
+  public:
+    int findKRotation(vector<int> &arr) {
+        // Code Here
+        int low=0; int high= arr.size()-1;
+        while(low<high){
+            int mid = low + (high - low) / 2;
+            if(arr[mid]>arr[high]) low=mid+1;
+            else high=mid;
+        }
+        return low;
+    }
+};
